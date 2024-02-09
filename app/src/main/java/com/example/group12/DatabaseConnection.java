1cff6375f75a25a1a2ba391655cbe6af85289b6a
Chaz Davies <ch460055@dal.ca>
2024-02-09 10:39:50 -0400

    database connection

---
+++ app/src/main/java/com/example/group12/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package com.example.group12;
+/*
+DatabaseConnection.java
+AUTHOR: Chaz Davies
+
+This file includes a class with all methods related to the database.
+ */
+public class DatabaseConnection {
+
+    public void setupDataBase(){
+
+    }
+
+}
