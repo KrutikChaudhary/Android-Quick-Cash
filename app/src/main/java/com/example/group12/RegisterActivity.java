3015ea0b7b18b602f94d20f9b26c79c0c92ac0fe
Mingxuan Chen <mn892709@dal.ca>
2024-02-09 17:38:41 +1300

    Registration validation implemented

--- app/src/main/java/com/example/group12/RegisterActivity.java
+++ app/src/main/java/com/example/group12/RegisterActivity.java
@@ -2,10 +2,12 @@ package com.example.group12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class RegisterActivity extends AppCompatActivity {
@@ -22,12 +24,67 @@ public class RegisterActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                cleanupLabels();
+                Validator validator = new Validator();
+                boolean validated = false;
+                String emailMessage = "";
+                String passwordMessage = "";
 
+                if (validator.isEmailEmpty(getEmail())){
+                    emailMessage = getResources().getString(R.string.EMAIL_EMPTY).trim();
+                }
+                else{
+                    if (validator.isValidEmailAddress(getEmail())){
+                        if (!validator.isPasswordEmpty(getPassword())){
+                            if (validator.isPasswordMatch(getPassword(), getConfirmedPassword())){
+                                if (validator.checkPasswordLength(getPassword())){
+                                    if (validator.isValidPassword(getPassword())){
+                                        validated = true;
+                                    }
+                                    else{
+                                        passwordMessage = getResources().getString(R.string.PASSWORD_INVALID).trim();
+                                    }
+                                }
+                                else{
+                                    passwordMessage = getResources().getString(R.string.PASSWORD_INVALID_LENGTH).trim();
+                                }
+                            }
+                            else{
+                                passwordMessage = getResources().getString(R.string.PASSWORD_MISMATCH);
+                            }
+                        }
+                        else{
+                            passwordMessage = getResources().getString(R.string.PASSWORD_EMPTY);
+                        }
+                    }
+                    //if email is not valid
+                    else{
+                        emailMessage = getResources().getString(R.string.EMAIL_INVALID).trim();
+                    }
+                }
+
+                if (validated){
+                    saveToFirebase();
+                    Intent selectRoleIntent = new Intent(RegisterActivity.this, SelectRoleActivity.class);
+                    RegisterActivity.this.startActivity(selectRoleIntent);
+                }
+                else{
+                    if (!emailMessage.isEmpty()){
+                        setEmailLabel(emailMessage);
+                    }
+                    else{
+                        setPasswordLabel(passwordMessage);
+                    }
+                }
             }
         });
     }
 
     protected void databaseInit(){
+
+    }
+
+    protected void saveToFirebase(){
 
     }
 
@@ -44,5 +101,20 @@ public class RegisterActivity extends AppCompatActivity {
     protected String getConfirmedPassword(){
         EditText confirmBox = findViewById(R.id.confirmPasswordText);
         return confirmBox.getText().toString().trim();
+    }
+
+    protected void setEmailLabel(String message){
+        TextView emailLabel = findViewById(R.id.emailLabel);
+        emailLabel.setText(message.trim());
+    }
+
+    protected void setPasswordLabel(String message){
+        TextView passwordLabel = findViewById(R.id.passwordLabel);
+        passwordLabel.setText(message.trim());
+    }
+
+    protected void cleanupLabels(){
+        setEmailLabel("");
+        setPasswordLabel("");
     }
 }
--- app/src/main/java/com/example/group12/Validator.java
+++ app/src/main/java/com/example/group12/Validator.java
@@ -8,17 +8,22 @@ public class Validator {
         return email.isEmpty();
     }
 
-    protected boolean isPasswordEmpty(String password1, String password2){
-        return password1.isEmpty() && password2.isEmpty();
+    protected boolean isPasswordEmpty(String password1){
+        return password1.isEmpty();
     }
 
+    /**
+     * Check if the email address is valid, should contain a '@' and a valid tld
+     * @param emailAddress
+     * @return
+     */
     protected boolean isValidEmailAddress(String emailAddress) {
-        //buggy method, fix it!
         String email_format = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,}$";
         Pattern email_pattern = Pattern.compile(email_format);
         Matcher matcher = email_pattern.matcher(emailAddress);
         return matcher.matches();
     }
+
 
     protected boolean isPasswordMatch(String p1, String p2){
         return p1.equals(p2);
@@ -28,6 +33,11 @@ public class Validator {
         return password.length() >= 7 && password.length() <= 16;
     }
 
+    /**
+     * check if a password is valid, should contain a capital letter and a number
+     * @param password
+     * @return
+     */
     protected boolean isValidPassword(String password){
         String passwordRegex = "^(?=.*[A-Z])(?=.*\\d).+$";
         Pattern passwordPattern = Pattern.compile(passwordRegex);
