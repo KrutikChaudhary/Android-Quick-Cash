516ff430f4057ea3ea705cbbac5dfe6ccac58e2a
YashRoushan <yashroushanprogramming@gmail.com>
2024-02-13 03:33:10 -0400

    Made the necessary XML and Java files for the Dashboard Navigation

---
+++ app/src/main/java/com/example/group12/Activity_Dashboard_User_FindJob.java
@@ -0,0 +1,14 @@
+package com.example.group12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Dashboard_User_FindJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_user_findjob);
+    }
+}
