e66836f8069d2f7d13962389ad2bae90b65643b0
yash roushan <yashroushan@gmail.com>
2024-02-13 03:44:28 -0400

    Appropriately renamed the java and xml files

--- app/src/main/java/com/example/group12/Dashboard_User_Profile.java
+++ app/src/main/java/com/example/group12/Dashboard_User_Profile.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity_Dashboard_User_Profile extends AppCompatActivity {
+public class Dashboard_User_Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
