0ef17e889870f1ebb682fd7d2dea791b5a7b7bb3
ankon <an656651@dal.ca>
2024-02-13 03:21:59 -0400

    Edited and made a new layout for the Employer Dashboard

--- app/src/main/java/com/example/group12/Dashboard_Employer.java
+++ app/src/main/java/com/example/group12/Dashboard_Employer.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Dashboard_Employee extends AppCompatActivity {
+public class Dashboard_Employer extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_employee);
+        setContentView(R.layout.activity_dashboard_employer);
     }
 }
