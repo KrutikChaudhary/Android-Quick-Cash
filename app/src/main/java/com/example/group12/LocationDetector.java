e6819d20676a53880cd7bda3a22912e50d4a6637
YashRoushan <yashroushanprogramming@gmail.com>
2024-02-15 09:59:20 -0400

    implemented location detector methods

--- app/src/main/java/com/example/group12/LocationDetector.java
+++ app/src/main/java/com/example/group12/LocationDetector.java
@@ -6,14 +6,22 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Looper;
 import android.location.Location;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class LocationDetector extends AppCompatActivity {
@@ -55,16 +63,27 @@ public class LocationDetector extends AppCompatActivity {
     }
 
     public void stopLocationUpdates(LocationCallback locationCallback) {
-        //IMPLEMENT
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
 
     public void onLocationUpdated(Location location) {
-        //IMPLEMENT
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        List<Address> addresses = null;
+        try {
+            addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Log.d("Address: ","Latitude :" + addresses.get(0).getLatitude() + "\nLongitude :" + addresses.get(0).getLongitude() +
+                "\nAddress :" + addresses.get(0).getAddressLine(0) + "\nCity :" + addresses.get(0).getLocality() +
+                "\nCountry :" + addresses.get(0).getCountryCode());
+
     }
 
 
     public void onLocationUpdateFailed() {
-        //IMPLEMENT
+        Log.e("DetectionError", "Failed to process location");
+
     }
 
     @Override
