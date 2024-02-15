e8c4c90c787dfb26fcc4f42e461b24b3d34bd0c2
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-02-15 09:52:02 -0400

    initialize location callback

--- app/src/main/java/com/example/group12/LocationDetector.java
+++ app/src/main/java/com/example/group12/LocationDetector.java
@@ -1,12 +1,18 @@
 package com.example.group12;
 
-
+import android.app.Activity;
 import android.content.Context;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.pm.PackageManager;
+import android.os.Looper;
 import android.location.Location;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
 
@@ -30,7 +36,22 @@ public class LocationDetector extends AppCompatActivity {
     }
 
     private void initializeLocationCallback() {
-        //IMPLEMENT
+        LocationCallback locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null && !locationResult.getLocations().isEmpty()) {
+                    onLocationUpdated(locationResult.getLocations().get(0));
+                } else {
+                    onLocationUpdateFailed();
+                }
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        } else {
+            ActivityCompat.requestPermissions((Activity) context, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+        }
     }
 
     public void stopLocationUpdates(LocationCallback locationCallback) {
