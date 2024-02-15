5ed753be3264f98bd652e8afb288815e29c36fa4
YashRoushan <yashroushanprogramming@gmail.com>
2024-02-15 10:01:41 -0400

    called an instance of locationDetector

--- app/src/main/java/com/example/group12/MainActivity.java
+++ app/src/main/java/com/example/group12/MainActivity.java
@@ -6,9 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationDetector locationDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationDetector = new LocationDetector(this);
     }
 }
