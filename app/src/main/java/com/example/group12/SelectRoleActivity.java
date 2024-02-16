4098405d5573c16da72e8d760637c8759f7b3558
Sarah Whyte <sr869233@dal.ca>
2024-02-15 23:04:44 -0400

    role to dashboard implementation

--- app/src/main/java/com/example/group12/SelectRoleActivity.java
+++ app/src/main/java/com/example/group12/SelectRoleActivity.java
@@ -2,7 +2,10 @@ package com.example.group12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectRoleActivity extends AppCompatActivity {
 
@@ -11,4 +14,31 @@ public class SelectRoleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_role);
     }
+
+    protected void employeeRole(){
+        Button employee = findViewById(R.id.selectEmployee);
+        Button employer = findViewById(R.id.selectEmployer);
+
+        employee.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                //connect to employee dashboard
+                Intent a = new Intent(SelectRoleActivity.this, DashBoard_User);
+                startActivity(a);
+                //add role to database
+            }
+        }
+    }
+    protected void employerRole(){
+    Button employer = findViewById(R.id.selectEmployer);
+
+        employer.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+            //connect to employer dashboard
+            Intent a = new Intent(SelectRoleActivity.this, DashBoard_Employer);
+            startActivity(a);
+            //add role to database
+
+        }
+    }
+}
 }
