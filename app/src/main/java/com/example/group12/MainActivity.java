88e2c3fa5d091eff1b9557bc9eb5eb047f0a2f25
Sarah Whyte <sr869233@dal.ca>
2024-02-16 00:47:29 -0400

    Toast test to main activity

--- app/src/main/java/com/example/group12/MainActivity.java
+++ app/src/main/java/com/example/group12/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         registerButtonSetup();
         loginButtonSetup();
+        Toast.makeText(this, "Changed page to employee dashboard", Toast.LENGTH_SHORT).show();
+
     }
 
     protected void registerButtonSetup(){
