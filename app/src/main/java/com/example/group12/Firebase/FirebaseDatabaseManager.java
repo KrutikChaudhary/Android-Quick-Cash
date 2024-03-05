727c9ca16c1348ac875b213fa51e527c7ad51e57
Mingxuan Chen <mn892709@dal.ca>
2024-03-06 11:05:09 +1300

    FirebaseDatabaseManage bug fixed. SelectRoleActivity.java refactored

--- app/src/main/java/com/example/group12/Firebase/FirebaseDatabaseManager.java
+++ app/src/main/java/com/example/group12/Firebase/FirebaseDatabaseManager.java
@@ -1,4 +1,6 @@
 package com.example.group12.Firebase;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.google.firebase.database.DataSnapshot;
@@ -51,7 +53,7 @@ public class FirebaseDatabaseManager
 
 
     protected void initializeDatabaseRefs() {
-        userRef = database.getReference("User");
+        userRef = database.getReference().child("User").push();
     }
 
     public void saveUserCredentialsToFirebase(String email, String password){
@@ -60,6 +62,30 @@ public class FirebaseDatabaseManager
         map.put("Password", password);
 
         userRef.setValue(map);
+    }
+
+    public void updateRole(String role, String userKey){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("User").child(userKey);
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Map<String, Object> user = (Map<String, Object>) snapshot.getValue();
+                if (user != null){
+                    user.put("Role", role);
+                    ref.setValue(user);
+                }
+                else{
+                    Log.e("null user", "User map is null");
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public DatabaseReference getUserRef(){
