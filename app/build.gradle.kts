f229b9c16d20fc9ff285c6e14ba321ce1eddd1f5
kchaudhary <kr954026@dal.ca>
2024-03-10 01:36:40 -0400

    google maps dependency and api key

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("com.google.gms.google-services")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -38,6 +39,9 @@ android {
             merges += "META-INF/LICENSE-notice.md"
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
 
 }
 
@@ -48,6 +52,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("androidx.test.uiautomator:uiautomator:2.2.0")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     implementation("com.google.android.gms:play-services-location:21.1.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
