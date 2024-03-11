e2d450f06fbf51bdd4d27098b6a32d84e64157fe
ankon <an656651@dal.ca>
2024-03-11 20:47:27 -0300

    Implemented the code on post job page to go back to employer dashboard page

--- app/src/main/java/com/example/group12/Dashboard_Employer_PostJob.java
+++ app/src/main/java/com/example/group12/Dashboard_Employer_PostJob.java
@@ -2,7 +2,12 @@ package com.example.group12;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.group12.ui.Dashboard_Employer;
 
 public class Dashboard_Employer_PostJob extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class Dashboard_Employer_PostJob extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_employer_post_job);
+        YourJobButtonSetup();
+    }
+
+    protected void YourJobButtonSetup() {
+        Button yourJobButton = findViewById(R.id.yourJobsButton);
+        yourJobButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent yourJobIntent = new Intent(Dashboard_Employer_PostJob.this, Dashboard_Employer.class);
+                Dashboard_Employer_PostJob.this.startActivity(yourJobIntent);
+            }
+        });
     }
 }
