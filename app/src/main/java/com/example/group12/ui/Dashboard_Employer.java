7ac13f9d1b9a18ff86893f6d48a1637e065b985b
YashRoushan <yashroushanprogramming@gmail.com>
2024-03-11 19:16:09 -0300

    implemented the postJob Button

--- app/src/main/java/com/example/group12/ui/Dashboard_Employer.java
+++ app/src/main/java/com/example/group12/ui/Dashboard_Employer.java
@@ -2,8 +2,12 @@ package com.example.group12.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.group12.Dashboard_Employer_PostJob;
 import com.example.group12.R;
 
 public class Dashboard_Employer extends AppCompatActivity {
@@ -12,5 +16,18 @@ public class Dashboard_Employer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_employer);
+        postJobButtonSetup();
+    }
+
+
+    protected void postJobButtonSetup(){
+        Button postJobButton = findViewById(R.id.postJobButton);
+        postJobButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(Dashboard_Employer.this, Dashboard_Employer_PostJob.class);
+                Dashboard_Employer.this.startActivity(loginIntent);
+            }
+        });
     }
 }
