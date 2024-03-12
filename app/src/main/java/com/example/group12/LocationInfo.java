4dcb1ada503b465e2b4e3ba39a6b1e0a0a0a666f
yash roushan <yashroushan@gmail.com>
2024-03-12 20:08:26 -0300

    location info class for separating data and firebase access

---
+++ app/src/main/java/com/example/group12/LocationInfo.java
@@ -0,0 +1,57 @@
+package com.example.group12;
+
+public class LocationInfo {
+    private double latitude;
+    private double longitude;
+    private String address;
+    private String locality;
+    private String countryCode;
+
+    public LocationInfo(double latitude, double longitude, String address, String locality, String countryCode) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.address = address;
+        this.locality = locality;
+        this.countryCode = countryCode;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+}
