56c4ae654f73bf683568bab77f30dd8e649b0783
Mingxuan Chen <mn892709@dal.ca>
2024-03-15 12:08:17 +1300

    test method commented out

--- app/src/main/java/com/example/group12/ui/MainActivity.java
+++ app/src/main/java/com/example/group12/ui/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity{
         setContentView(R.layout.activity_main);
         registerButtonSetup();
         loginButtonSetup();
-        testJob();
+        //testJob();
     }
 
     protected void registerButtonSetup(){
