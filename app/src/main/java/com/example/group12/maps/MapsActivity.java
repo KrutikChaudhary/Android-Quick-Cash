8c138f1cd6d584ba1759227b3f5864ee30424e1b
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-03-18 09:08:53 -0300

    maps activity setup

---
+++ app/src/main/java/com/example/group12/maps/MapsActivity.java
@@ -0,0 +1,28 @@
+package com.example.group12.maps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.group12.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap googleMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+}
