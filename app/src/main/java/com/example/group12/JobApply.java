1d17c3c3eabc887b96ed3a7d10794accc0a9efac
Krutik Chaudhary <krutik@ta9ee.wpa.dal.ca>
2024-03-18 22:24:41 -0300

    apply if merchant id connected

--- app/src/main/java/com/example/group12/JobApply.java
+++ app/src/main/java/com/example/group12/JobApply.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,6 +16,8 @@ import com.example.group12.R;
 import com.example.group12.core.Constants;
 import com.example.group12.ui.Dashboard_User;
 import com.example.group12.ui.LogInActivity;
+import com.example.group12.util.MerchantIDCallBack;
+import com.example.group12.util.MerchantIDValidator;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -44,15 +47,29 @@ public class JobApply extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = fullName.getText().toString();
-                if(name!=null&&!name.equals("")){
-                    saveJobApplicationToFirebase(email, name, merchantID);
-                    Intent intent = new Intent(JobApply.this, Dashboard_User.class);
-                    intent.putExtra("email",email);
-                    Toast.makeText(JobApply.this, "Application Successful", Toast.LENGTH_SHORT).show();
-                    JobApply.this.startActivity(intent);
-                } else {
-                    Toast.makeText(JobApply.this, "Please add your Full name", Toast.LENGTH_SHORT).show();
-                }
+                MerchantIDValidator validator = new MerchantIDValidator();
+                validator.isMerchantIDConnected(email, new MerchantIDCallBack() {
+                    @Override
+                    public void merchantIdAvailableResult(boolean isValid, String merchantID) {
+                        if(isValid){
+                            if(name!=null&&!name.equals("")){
+                                saveJobApplicationToFirebase(email, name, merchantID);
+                                Intent intent = new Intent(JobApply.this, Dashboard_User.class);
+                                intent.putExtra("email",email);
+                                Log.d("JobAdapter", "Email received: " + email);
+                                Toast.makeText(JobApply.this, "Application Successful", Toast.LENGTH_SHORT).show();
+                                JobApply.this.startActivity(intent);
+                            } else {
+                                Toast.makeText(JobApply.this, "Please add your Full name", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Intent applyIntent = new Intent(JobApply.this, Dashboard_User_MyPayPal.class);
+                            applyIntent.putExtra("email", email);
+                            Toast.makeText(JobApply.this, "Add your Merchant ID first", Toast.LENGTH_SHORT).show();
+                            JobApply.this.startActivity(applyIntent);
+                        }
+                    }
+                });
             }
         });
     }
