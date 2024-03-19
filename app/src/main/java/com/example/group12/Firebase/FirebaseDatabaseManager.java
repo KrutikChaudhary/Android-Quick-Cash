259c014619e5f554db1319bfed5d8f0f0146f385
ankon <an656651@dal.ca>
2024-03-19 20:51:35 -0300

    properly commented firebase database manager class

--- app/src/main/java/com/example/group12/Firebase/FirebaseDatabaseManager.java
+++ app/src/main/java/com/example/group12/Firebase/FirebaseDatabaseManager.java
@@ -12,14 +12,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
-
-/*
-DatabaseConnection.java
-AUTHOR: Chaz Davies
-
-This file includes a class with all methods related to the database.
- */
-
 import com.example.group12.logic.FilterJob;
 import com.example.group12.model.Job;
 import com.example.group12.util.JobFilterCallback;
@@ -27,12 +19,11 @@ import com.example.group12.util.JobFilterCallback;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
-DatabaseConnection.java
-AUTHOR: Chaz Davies
-This file includes a class with all methods related to the database.
-*/
-
+/**
+ * FirebaseDatabaseManager class contains methods related to database operations.
+ * This includes saving user credentials, job details, merchant IDs, job applications,
+ * user locations, and updating user roles in Firebase Realtime Database.
+ */
 public class FirebaseDatabaseManager
 {
     private FirebaseDatabase database;
@@ -42,27 +33,23 @@ public class FirebaseDatabaseManager
     private DatabaseReference userLocation;
     private DatabaseReference jobRef;
 
+    /**
+     * Default constructor.
+     */
     public FirebaseDatabaseManager(){}
+
+    /**
+     * Parameterized constructor to initialize FirebaseDatabaseManager with a specified database instance.
+     * @param database FirebaseDatabase instance
+     */
     public FirebaseDatabaseManager(FirebaseDatabase database) {
         this.database = database;
-//this.setDBListener();
         this.initializeDatabaseRefs();
     }
-// protected void setDBListener() {
-// this.db_ref.addValueEventListener(new ValueEventListener() {
-// @Override
-// public void onDataChange(@NonNull DataSnapshot snapshot) {
-// //incomplete method, add your implementation
-// extractedMessage = snapshot.getValue(String.class);
-// }
-//
-// @Override
-// public void onCancelled(@NonNull DatabaseError error) {
-//
-// }
-// });
-// }
 
+    /**
+     * Initializes database references.
+     */
     protected void initializeDatabaseRefs() {
         userRef = database.getReference().child("User");
         userLocation = database.getReference().child("Location");
@@ -71,6 +58,13 @@ public class FirebaseDatabaseManager
         jobApplicationRef = database.getReference().child("Job Application");
 
     }
+
+    /**
+     * Saves user credentials to Firebase database.
+     * @param email User email
+     * @param password User password
+     * @return DatabaseReference for the saved user credentials
+     */
     public DatabaseReference saveUserCredentialsToFirebase(String email, String password){
         Map<String, Object> map = new HashMap<>();
         map.put("Email", email);
@@ -80,6 +74,17 @@ public class FirebaseDatabaseManager
         return dbref;
     }
 
+    /**
+     * Saves job details to Firebase database.
+     * @param jobTitle Title of the job
+     * @param date Start date of the job
+     * @param expectedDuration Expected duration of the job
+     * @param urgency Urgency of the job
+     * @param salary Salary for the job
+     * @param jobLocation Location of the job
+     * @param latitude Latitude of the job location
+     * @param longitude Longitude of the job location
+     */
     public void saveJobsToFirebase(String jobTitle, String date, int expectedDuration, String urgency,
                                    float salary, String jobLocation, float latitude, float longitude){
         Map<String, Object> map = new HashMap<>();
@@ -96,6 +101,12 @@ public class FirebaseDatabaseManager
         dbref.setValue(map);
     }
 
+    /**
+     * Saves merchant ID to Firebase database.
+     * @param email Merchant email
+     * @param merchantID Merchant ID
+     * @return DatabaseReference for the saved merchant ID
+     */
     public DatabaseReference saveMerchantIDtoFirebase(String email,String merchantID){
         Map<String, Object> map = new HashMap<>();
         map.put("Email", email);
@@ -106,6 +117,13 @@ public class FirebaseDatabaseManager
         return dbref;
     }
 
+    /**
+     * Saves job application details to Firebase database.
+     * @param email User email
+     * @param name User name
+     * @param merchantID Merchant ID
+     * @return DatabaseReference for the saved job application
+     */
     public DatabaseReference saveJobApplicationToFirebase(String email,String name, String merchantID){
         Map<String, Object> map = new HashMap<>();
         map.put("Email", email);
@@ -118,11 +136,9 @@ public class FirebaseDatabaseManager
     }
 
 
-    /*
-     * This method saves the location details to firebase
-     *
-     * @param locationInfo Location info objects which stores the location
-     * @return void
+    /**
+     * Saves location details to Firebase database.
+     * @param locationInfo LocationInfo object containing location details
      */
     public void saveLocationToFirebase(LocationInfo locationInfo){
         Map<String, Object> locationMap = new HashMap<>();
@@ -134,42 +150,86 @@ public class FirebaseDatabaseManager
         DatabaseReference dbref = this.userLocation.push();
         dbref.setValue(locationMap);
     }
-    public void updateRole(String role, String userKey){
+
+    /**
+     * Updates user role in Firebase database.
+     * @param role New role to be updated
+     * @param userKey Key of the user in the database
+     */
+    public void updateRole(String role, String userKey) {
+        // Get DatabaseReference for the user's role under the "User" node in the Firebase database
         DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("User").child(userKey);
+
+        // Listen for a single value event to retrieve the user's current role
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Retrieve user data as a Map from the DataSnapshot
                 Map<String, Object> user = (Map<String, Object>) snapshot.getValue();
-                if (user != null){
+
+                // Check if user data is not null
+                if (user != null) {
+                    // Update the user's role with the new role value
                     user.put("Role", role);
+
+                    // Set the updated user data back to the database
                     ref.setValue(user);
-                }
-                else{
+                } else {
+                    // Log an error message if user data is null
                     Log.e("null user", "User map is null");
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                // Handle database error if any
             }
         });
     }
+
+    /**
+     * Retrieves DatabaseReference for User node.
+     * @return DatabaseReference for User node
+     */
     public DatabaseReference getUserRef(){
         return this.userRef;
     }
+
+    /**
+     * Retrieves DatabaseReference for MerchantID node.
+     * @return DatabaseReference for MerchantID node
+     */
     public DatabaseReference getMerchantRef(){
         return this.merchantRef;
     }
 
+    /**
+     * Retrieves DatabaseReference for Job node.
+     * @return DatabaseReference for Job node
+     */
     public DatabaseReference getJobRef(){
         return this.jobRef;
     }
+
+    /**
+     * Filters jobs based on specified parameters.
+     * @param parameter Parameter to filter jobs
+     * @param salary Salary range to filter jobs
+     * @param duration Duration range to filter jobs
+     * @param distance Distance range to filter jobs
+     * @param callback Callback to handle filtered jobs
+     */
     public void jobFilter(String parameter, String salary, String duration, String distance, JobFilterCallback callback){
+        // Create a list to store filtered jobs
         List<Job> filterdJobList = new ArrayList<>();
+        // Retrieve DatabaseReference for Job node and listen for a single value event
         this.getJobRef().addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
+                // Iterate through each child node under the Job node
                 for (DataSnapshot jobSnapshot : snapshot.getChildren()){
+                    // Extract job details from the DataSnapshot
                     Map<String, Object> jobMap = (Map<String, Object>) jobSnapshot.getValue();
                     String jobTitle = (String) jobMap.get("title");
                     float jobSalary = (float) jobMap.get("salary");
@@ -180,26 +240,24 @@ public class FirebaseDatabaseManager
                     float jobLongitude = (float) jobMap.get("longitude");
                     float jobLatitude = (float) jobMap.get("longitude");
 
-                    // getUser location
-
-
+                    // Create a new instance of FilterJob to perform filtering
                     FilterJob filterJob = new FilterJob();
 
-
-
+                    // Check if the job satisfies the filtering criteria
                     if (filterJob.containsParameters(parameter, jobTitle) && filterJob.containsSalary(salary, jobSalary) && filterJob.containsDuration(duration, jobDuration)){
+                        // If the job meets the criteria, create a Job object and add it to the filtered job list
                         Job job = new Job(jobTitle, jobSalary, jobDuration, jobStartDate, jobLocation, jobUrgency, jobLatitude, jobLongitude);
                         filterdJobList.add(job);
                     }
 
+                    // Pass the filtered job list to the callback function
                     callback.onJobFilterSuccess(filterdJobList);
                 }
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+                // Handle database error if any
             }
         });
     }
-
-
 }
