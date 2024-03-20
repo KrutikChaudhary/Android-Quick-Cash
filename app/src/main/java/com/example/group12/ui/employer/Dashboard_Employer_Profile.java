d2ce733da33fdd9ab6d112e61b4059c5137a8385
ankon <an656651@dal.ca>
2024-03-19 22:18:19 -0300

    commented code and javadoc for ui employer folder classes

--- app/src/main/java/com/example/group12/ui/employer/Dashboard_Employer_Profile.java
+++ app/src/main/java/com/example/group12/ui/employer/Dashboard_Employer_Profile.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 import com.example.group12.R;
 
+/**
+ * Activity class for the employer profile dashboard.
+ * This activity displays the profile information of the employer.
+ */
 public class Dashboard_Employer_Profile extends AppCompatActivity {
 
     @Override
