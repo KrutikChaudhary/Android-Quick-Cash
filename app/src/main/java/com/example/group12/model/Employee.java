e9d28b2fe4363aca61181e6926a2ab52e084546b
ankon <an656651@dal.ca>
2024-03-19 21:28:20 -0300

    Wrote proper comments and javadoc for the model classes

--- app/src/main/java/com/example/group12/model/Employee.java
+++ app/src/main/java/com/example/group12/model/Employee.java
@@ -2,10 +2,16 @@ package com.example.group12.model;
 
 import com.example.group12.model.User;
 
+/**
+ * Represents an employee, extending the User class.
+ */
 public class Employee extends User {
 
+    /**
+     * Default constructor.
+     */
     public Employee(){
-        super();
+        super(); // Call the constructor of the superclass (User)
     }
 
 }
