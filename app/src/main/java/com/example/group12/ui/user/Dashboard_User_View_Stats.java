e4ef98964afdffc976e9858fa22c98de4f4ddb5a
ankon <an656651@dal.ca>
2024-03-30 05:31:34 -0300

    added test methods for employee visualize stats page

--- app/src/main/java/com/example/group12/ui/user/Dashboard_User_View_Stats.java
+++ app/src/main/java/com/example/group12/ui/user/Dashboard_User_View_Stats.java
@@ -9,7 +9,7 @@ import com.example.group12.R;
 /**
  * Activity class for displaying the user profile in the user dashboard.
  */
-public class Dashboard_User_Preffered_Jobs extends AppCompatActivity {
+public class Dashboard_User_Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
