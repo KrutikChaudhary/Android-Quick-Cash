9b7ddc288a911f282f83703d2e81cee985c89677
ankon <an656651@dal.ca>
2024-03-30 05:13:20 -0300

    Created the layout for employer statistics page and the button design on the employer page

--- app/src/main/java/com/example/group12/ui/user/Dashboard_User_Profile.java
+++ app/src/main/java/com/example/group12/ui/user/Dashboard_User_Profile.java
@@ -9,11 +9,11 @@ import com.example.group12.R;
 /**
  * Activity class for displaying the user profile in the user dashboard.
  */
-public class Dashboard_User_Profile extends AppCompatActivity {
+public class Dashboard_User_Preffered_Jobs extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard_user_profile);
+        setContentView(R.layout.activity_dashboard_user_preffered_jobs);
     }
 }
