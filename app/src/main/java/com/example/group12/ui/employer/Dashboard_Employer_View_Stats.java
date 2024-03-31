f7569480c9b7217bb2fe13a3712ccad5e62d0eb5
ankon <an656651@dal.ca>
2024-03-31 16:27:40 -0300

    code implemented for the employer statistics page

--- app/src/main/java/com/example/group12/ui/employer/Dashboard_Employer_View_Stats.java
+++ app/src/main/java/com/example/group12/ui/employer/Dashboard_Employer_View_Stats.java
@@ -2,19 +2,39 @@ package com.example.group12.ui.employer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.example.group12.R;
 
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+
 /**
- * Activity class for the employer profile dashboard.
- * This activity displays the profile information of the employer.
+ * Activity class for displaying the employer profile in the user dashboard.
  */
 public class Dashboard_Employer_View_Stats extends AppCompatActivity {
+    PieChart pieChart;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_employer_view_stats);
+        pieChart = findViewById(R.id.chart);
+
+        pieChart.addPieSlice(
+                new PieModel(
+                        "R",
+                        17,
+                        Color.parseColor("#FFA726")));
+
+        pieChart.addPieSlice(
+                new PieModel(
+                        "Python",
+                        6,
+                        Color.parseColor("#66BB6A")));
+        pieChart.startAnimation();
     }
 }
