06fd42de71758bcd3ecc0c79eb115f25d38788e5
kchaudhary <kr954026@dal.ca>
2024-04-04 22:53:06 -0300

    made changes according to Code Review

--- app/src/main/java/com/example/group12/logic/ApplicationValidator.java
+++ app/src/main/java/com/example/group12/logic/ApplicationValidator.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ApplicationValidator {
+    private final String email_format = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
     public boolean isEmailEmpty(String email){
         return email.isEmpty();
@@ -17,7 +18,6 @@ public class ApplicationValidator {
 
 
     public boolean isValidEmail(String email) {
-        String email_format = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
         Pattern email_pattern = Pattern.compile(email_format);
         Matcher matcher = email_pattern.matcher(email);
         return matcher.matches();
