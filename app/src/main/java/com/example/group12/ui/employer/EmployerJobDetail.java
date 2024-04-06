303e128afebdff8d5b06d430629c9b004e66a04b
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-04-06 05:04:09 -0300

    job detail activity

---
+++ app/src/main/java/com/example/group12/ui/employer/EmployerJobDetail.java
@@ -0,0 +1,16 @@
+package com.example.group12.ui.employer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.group12.R;
+
+public class EmployerJobDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employer_job_detail);
+    }
+}
