b68d5bd83c66ada929eb6e01dedb9c7b8b5b9f96
YashRoushan <yashroushanprogramming@gmail.com>
2024-04-07 01:19:37 -0300

    refactored CreditCardValidator to follow LSP solid principle

--- app/src/main/java/com/example/group12/logic/cardValidator/CreditCardValidator.java
+++ app/src/main/java/com/example/group12/logic/cardValidator/CreditCardValidator.java
@@ -10,17 +10,7 @@ public class CreditCardValidator {
 
     public static final int CARDLENGTHFLOOR = 13; // Minimum length of a credit card number
     public static final int CARDLENGTHCEILING = 16; // Maximum length of a credit card number
-    public static final int CVVLENGTH = 3; // Length of CVV number
 
-    /**
-     * Checks if the CVV number is empty.
-     *
-     * @param cvv The CVV number to be checked.
-     * @return true if the CVV number is empty, false otherwise.
-     */
-    public boolean isCVVEmpty(String cvv){
-        return cvv.isEmpty();
-    }
 
     /**
      * Checks if the credit card number is empty.
@@ -55,18 +45,6 @@ public class CreditCardValidator {
         return matcher.matches();
     }
 
-    /**
-     * Checks if the CVV number is valid based on a regex pattern.
-     *
-     * @param cvv The CVV number to be validated.
-     * @return true if the CVV number is valid, false otherwise.
-     */
-    public boolean isValidCVV(String cvv) {
-        String cvv_format = ""; // Regular expression pattern for validating CVV numbers
-        Pattern cvv_pattern = Pattern.compile(cvv_format);
-        Matcher matcher = cvv_pattern.matcher(cvv);
-        return matcher.matches();
-    }
 
     /**
      * Checks if the expiry date is valid based on a regex pattern.
@@ -87,17 +65,8 @@ public class CreditCardValidator {
      * @param cardNumber The credit card number to be checked.
      * @return true if the length of the credit card number is valid, false otherwise.
      */
-    public boolean checkCardLength(String cardNumber){
+    public boolean checkCardLength(String cardNumber) {
         return cardNumber.length() == CARDLENGTHFLOOR || cardNumber.length() == CARDLENGTHCEILING;
     }
 
-    /**
-     * Checks if the length of the CVV number is valid.
-     *
-     * @param cvv The CVV number to be checked.
-     * @return true if the length of the CVV number is valid, false otherwise.
-     */
-    public boolean checkCVVLength(String cvv){
-        return cvv.length() == CVVLENGTH;
-    }
 }
