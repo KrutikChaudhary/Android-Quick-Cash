f2fc75a1e080cb97b85a60250485dd49b24d4416
KrutikChaudhary <krutikchaudhary39@gmail.com>
2024-04-07 06:22:40 -0300

    shifted getters and job filter to database read class

--- app/src/main/java/com/example/group12/logic/LoginValidator.java
+++ app/src/main/java/com/example/group12/logic/LoginValidator.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.example.group12.firebase.FirebaseDatabaseManager;
+import com.example.group12.firebase.FirebaseReadManager;
 import com.example.group12.util.LoginCallback;
 import com.example.group12.core.Constants;
 import com.google.firebase.database.DataSnapshot;
@@ -20,7 +21,7 @@ import java.util.Map;
  * Firebase user data.
  */
 public class LoginValidator {
-    FirebaseDatabaseManager dbManager;
+    FirebaseReadManager dbManager;
 
     private boolean valid = false;
     private String role = "";
@@ -32,7 +33,7 @@ public class LoginValidator {
      * Initializes the FirebaseDatabaseManager instance.
      */
     public LoginValidator(){
-        dbManager = new FirebaseDatabaseManager(FirebaseDatabase.getInstance(Constants.FIREBASE_LINK));
+        dbManager = new FirebaseReadManager(FirebaseDatabase.getInstance(Constants.FIREBASE_LINK));
     }
 
     /**
