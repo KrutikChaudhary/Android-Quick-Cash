50fbdaa617806d1c62388d3b4709fe68b7e07bc8
yash roushan <yashroushan@gmail.com>
2024-04-07 01:20:17 -0300

    CvvValidator

--- app/src/main/java/com/example/group12/logic/cardValidator/CvvValidator.java
+++ app/src/main/java/com/example/group12/logic/cardValidator/CvvValidator.java
@@ -1,2 +1,35 @@
-package com.example.group12.logic.cardValidator;public class CvvValidator {
+package com.example.group12.logic.cardValidator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CvvValidator {
+    public static final int CVVLENGTH = 3; // Length of CVV number
+
+    /**
+     * Checks if the length of the CVV number is valid.
+     * @return true if the length of the CVV number is valid, false otherwise.
+     */
+    public boolean checkCVVLength(String cvv){
+        return cvv.length() == CVVLENGTH;
+    }
+
+    /**
+     * Checks if the CVV number is empty.
+     * @return true if the CVV number is empty, false otherwise.
+     */
+    public boolean isCVVEmpty(String cvv){
+        return cvv.isEmpty();
+    }
+
+    /**
+     * Checks if the CVV number is valid based on a regex pattern.
+     * @return true if the CVV number is valid, false otherwise.
+     */
+    public boolean isValidCVV(String cvv) {
+        String cvv_format = "\\\\d{3}"; // Regular expression pattern for validating CVV numbers
+        Pattern cvv_pattern = Pattern.compile(cvv_format);
+        Matcher matcher = cvv_pattern.matcher(cvv);
+        return matcher.matches();
+    }
 }
