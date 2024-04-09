935be71b183390c6d614fecfe49f9696593855dd
kchaudhary <kr954026@dal.ca>
2024-04-09 00:06:04 -0300

    java doc comments

---
+++ app/src/main/java/com/example/group12/util/statsCount/StatsCountStrategy.java
@@ -0,0 +1,14 @@
+package com.example.group12.util;
+
+/**
+ * This interface defines a strategy for calculating total counts associated with a specific user's email address.
+ */
+public interface StatsCountStrategy {
+    /**
+     * Retrieves the total counts associated with the provided email address.
+     *
+     * @param email    The email address of the user for whom the counts are being retrieved.
+     * @param callback A callback to handle the result of the count retrieval operation.
+     */
+    public void getTotalCounts(String email, FirebaseCountCallback callback);
+}
